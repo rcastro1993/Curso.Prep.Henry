10db50899f68cdde09655753ef6c61d3d9f2e47d
rcastro1993 <106711804+rcastro1993@users.noreply.github.com>
2022-06-23 11:03:01 -0300

    Homework I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -248,7 +248,7 @@ function esVocal(letra) {
   if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
     return "Es vocal"
   }
-  return "Dato incorrecto"
+  return "Dato incorrecto" 
 
 }
 
